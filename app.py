91fb8820199b4529af06ee663cfc0331c83c29ba
Antonio Cozzolino <cozzolinoac11@gmail.com>
2024-11-27 10:48:59 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import gradio as gr
+import bot
+
+kb_bot = bot.KnowledgeBaseBot(persist_directory='./kb_chroma_db_v2')
+
+# Set up chatbot interface
+answer_bot = gr.ChatInterface(kb_bot.process_chat,
+                              chatbot=gr.Chatbot(type="tuples"),
+                              textbox=gr.Textbox(placeholder="Type your question about the FAIRiCUBE project or "
+                                                             "knowledge base here...", container=False,
+                                                 scale=7),
+                              title="FAIRiCUBE Knowledge Base ChatBot",
+                              description="The FAIRiCUBE Knowledge Base ChatBot is an intelligent assistant designed "
+                                          "to help understand the FAIRiCUBE project. This chatbot acts as a guide to "
+                                          "FAIRiCUBE’s resources, answering questions about its objectives, "
+                                          "methodologies, and knowledge base. Whether you’re a researcher, "
+                                          "policymaker, or developer, the ChatBot provides insights into FAIRiCUBE’s "
+                                          "tools, use cases, analysis and processing resources, datasets, "
+                                          "and applications in environmental and spatial data science. With an "
+                                          "intuitive interface, it efficiently retrieves project information, "
+                                          "clarifies technical aspects, and directs users to relevant documents and "
+                                          "data sources within the FAIRiCUBE knowledge base. "
+                                          "\n\nUse the text box at the bottom of the page to ask your question about the FAIRiCUBE project and the Knowledge Base.",
+                              theme="soft",
+                              stop_btn="Interrupt",
+                              submit_btn="Ask",
+                              type="tuples"
+                              )
+
+answer_bot.queue(default_concurrency_limit=None)
+answer_bot.launch(share=True)
